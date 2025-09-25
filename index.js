baa0d17edf3eb0181b5079d5ca5255be258e1a1d
allanndunda <allanndunda>
2025-09-25 15:16:44 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  })
+  .then(response => response.json())
+  .then(data => {
+    // append the id value to the DOM
+    document.body.innerHTML = data.id;
+  })
+  .catch(error => {
+    // append the error message to the DOM
+    document.body.innerHTML = error.message;
+  });
+}
